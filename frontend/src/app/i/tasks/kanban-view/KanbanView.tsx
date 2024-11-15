81739ba7388cec2547b4c1fa1276b2f37bc02ef4
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2024-11-15 15:42:36 +0300

    added kanban desk frontend

--- frontend/src/app/i/tasks/kanban-view/KanbanView.tsx
+++ frontend/src/app/i/tasks/kanban-view/KanbanView.tsx
@@ -1,5 +1,30 @@
 'use client'
 
+import { DragDropContext } from "@hello-pangea/dnd";
+import { useTaskDnd } from "../hooks/useTaskDnd";
+import { useTasks } from "../hooks/useTasks";
+
+import styles from './KanbanView.module.scss';
+import { COLUMNS } from "../colums.data";
+import { KanbanColumn } from "./KanbanColumn";
+
 export function KanbanView() {
-  return <div>KanbanView</div>;
+  const { items, setItems } = useTasks();
+  const { onDragEnd } = useTaskDnd();
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className={styles.board}>
+        {COLUMNS.map(column => (
+          <KanbanColumn
+            key={column.value}
+            value={column.value}
+            label={column.label}
+            items={items}
+            setItems={setItems}
+          />
+        ))}
+      </div>
+    </DragDropContext>
+  );
 }
